d2e51fa34a2f4a3fec83b2b7791cdd875e50d718
Jonathan Beyrak-Lev <jonathan@eatwoodspoon.com>
2021-10-06 09:18:54 +0300

    first commit

---
+++ Cat/Shared/CatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatApp.swift
+//  Shared
+//
+//  Created by Jonathan Beyrak-Lev on 06/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
